59c6cdf28a5aac9cd2e098bf6fd5dd0aa162ecbd
ronngo <ronngomn23@gmail.com>
2020-09-08 21:25:43 +0700

    first commit

---
+++ 20songuyento/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _20songuyento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("20 so nguyen to dau tien la:");
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= 20)
+                    {
+                        Console.WriteLine(i + "\t");
+                    }
+                }
+            }
+        }
+    }
+}
